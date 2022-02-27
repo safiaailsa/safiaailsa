168eb3d727d467162566074970628c6b69fd1e9d
safiaailsa <100075671+safiaailsa@users.noreply.github.com>
2022-02-27 23:17:59 +0700

    Add files via upload

---
+++ ComputeMethods5026211128.java
@@ -0,0 +1,11 @@
+class ComputeMethods5026211128{
+
+  public void FtoC(double degreesF){
+	System.out.println("Temp in celcius is "+((degreesF-32)*5/9));
+  }
+
+  public void hypotenuse(int a, int b){
+	System.out.println("Hypotenuse is "+Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)));
+  }
+
+}
---
+++ TestClass5026211128.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+class TestClass5026211128{ 
+
+  public static void main (String[] args){ 
+	ComputeMethods5026211128 cm = new ComputeMethods5026211128();
+	cm.FtoC(50);
+	cm.hypotenuse(3, 4);
+
+  }
+
+}
